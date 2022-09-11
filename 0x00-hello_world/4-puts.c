24df13c79bce5866eae42d913f3a4e0f21124943
emzzy <ezehemmanuel265@gmail.com>
2022-09-11 02:26:48 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-/**
-* main-Entry point
-* Return: 0 if successful
+/*
+*main-Entry point
+*Return: 0 if successful
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("!\"Programming is like building a multilingual puzzle");
 return (0);
 }
