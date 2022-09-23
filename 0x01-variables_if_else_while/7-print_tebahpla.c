723decf574166562e7467bbc49b6a630ae20453b
root <emzzy>
2022-09-22 18:54:28 -0700

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-*main-print all single digits numbers of base 10
-*starting from 0, followed by a new line, using putchar
+*main-print the lowercase alphabet in reverse
+*followed by a new line
 *Return: 0 if successful
- */
+*/
 int main(void)
 {
-int n;
-for (n = 48; n < 58; n++)
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
 {
-putchar(n);
+putchar(ch);
 }
 putchar('\n');
 return (0);
