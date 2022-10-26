da14b05d3a53fad8849615b53663cfc197fb7438
root <emzzy>
2022-10-26 07:36:48 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,10 @@
 *main - Test functions for positive or negative
 *Return: 0
 */
-int main (void)
+int main(void)
 {
-  int i;
-  i = 0;
-  positive_or_negative(i);
-  return 0;
+int i;
+i = 0;
+positive_or_negative(i);
+return 0;
 }
