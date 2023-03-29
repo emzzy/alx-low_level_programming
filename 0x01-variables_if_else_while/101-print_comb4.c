104842f6bf89e8a78989f57358a7b9d46f7da648
emzzy <ezehemmanuel265@gmail.com>
2023-03-29 01:05:15 +0000

    a program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 
 /**
- *main- prints all possible different combinations of three digits
- *numbers must be seperated by comma and a space
- *Returns: zero
- */
+*main- prints all possible different combinations of three digits
+*numbers must be seperated by comma and a space
+*Returns: zero
+*/
 int main()
 {
-  int a, b, c;
-  a = 48;
-  b = 48;
-  c = 48;
-  for (a = 48; a < 58; a++)
-    {
-      for (b = 48; b < 58; b++)
-	{
-	  for (c = 48; c < 58; c++)
-	    {
-	      if (a != b && a < b)
-		{
-		  if (a != c && a < c)
-		    {
-		      if (b != c && b < c)
-			{
-			  putchar(a);
-			  putchar(b);
-			  putchar(c);
-			  putchar(',');
-			  putchar(' ');
-			}
-		    }
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int a, b, c;
+a = 48;
+b = 48;
+c = 48;
+for (a = 48; a < 58; a++)
+{
+for (b = 48; b < 58; b++)
+{
+for (c = 48; c < 58; c++)
+{
+if (a != b && a < b)
+{
+if (a != c && a < c)
+{
+if (b != c && b < c)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
